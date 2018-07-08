e44357750cd8a5b3de82a7d8ac275012f3c3d65f
oguzhanvrl <oguzhanvarol@outlook.com.tr>
2018-07-08 19:25:25 +0300

    tr controller and views update, template fragmented clear

---
+++ SportsHallWebApplication/SportsHallWebApplication/Controllers/trController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SportsHallWebApplication.Controllers
+{
+    public class trController : Controller
+    {
+        /*
+         * Renk Paleti
+        mavi :           #00549A
+        kırmızı :        #DA214D
+        açık mavi :      #8490ff
+        gri :            #777777
+        açıkgri :        #8a8a8a
+        blogmavi :       #8490ff
+
+        */
+
+        // GET: tr
+        public ActionResult Anasayfa()
+        {
+            return View();
+        }
+
+        public ActionResult KayitOl()
+        {
+            return View();
+        }
+
+        public ActionResult Blog()
+        {
+            return View();
+        }
+
+        public ActionResult Hakkimizda()
+        {
+            return View();
+        }
+
+        public ActionResult Galeri()
+        {
+            return View();
+        }
+
+        public ActionResult Iletisim()
+        {
+            return View();
+        }
+
+        public ActionResult Panel()
+        {
+            return View();
+        }
+    }
+}
